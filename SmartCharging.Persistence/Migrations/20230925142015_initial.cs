939db051b633fa0f30971b14e420320228a556e5
ddemirci <doruk.demirci@ichoosr.com>
2023-09-27 15:22:48 +0200

    add the initial migration

---
+++ SmartCharging.Persistence/Migrations/20230925142015_initial.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartCharging.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false),
+                    Name = table.Column<string>(type: "longtext", nullable: false),
+                    CapacityInAmps = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ChargeStations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false),
+                    Name = table.Column<string>(type: "longtext", nullable: false),
+                    GroupId = table.Column<Guid>(type: "char(36)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChargeStations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChargeStations_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Connectors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    ChargeStationId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    MaxCurrentInAmps = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Connectors", x => new { x.Id, x.ChargeStationId });
+                    table.ForeignKey(
+                        name: "FK_Connectors_ChargeStations_ChargeStationId",
+                        column: x => x.ChargeStationId,
+                        principalTable: "ChargeStations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChargeStations_GroupId",
+                table: "ChargeStations",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connectors_ChargeStationId",
+                table: "Connectors",
+                column: "ChargeStationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Connectors");
+
+            migrationBuilder.DropTable(
+                name: "ChargeStations");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+        }
+    }
+}
