bbf26946d92905396332e1ae734ee61843adbb97
ddemirci <doruk.demirci@ichoosr.com>
2023-09-27 15:22:48 +0200

    update `IRepository`

--- SmartCharging.Repository/IRepository.cs
+++ SmartCharging.Repository/IRepository.cs
@@ -3,7 +3,7 @@ namespace SmartCharging.Repository;
 public interface IRepository<T>
 {
     Task<T> Get(Guid id, CancellationToken ct);
-    void Add(T entity, CancellationToken ct);
-    void Update(T entity, CancellationToken ct);
-    void Delete(T entity, CancellationToken ct);
+    Task<T> Add(T entity, CancellationToken ct);
+    T Update(T entity, CancellationToken ct);
+    T Delete(T entity, CancellationToken ct);
 }
