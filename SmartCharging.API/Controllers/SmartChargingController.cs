d818375ecda001e2cf8af298c47febcfd1ce7e83
ddemirci <doruk.demirci@ichoosr.com>
2023-09-29 17:50:44 +0200

    Instead of `SmartChargingApiResponse`, use `IResult`

--- SmartCharging.API/Controllers/SmartChargingController.cs
+++ SmartCharging.API/Controllers/SmartChargingController.cs
@@ -3,8 +3,6 @@ using SmartCharging.API.Manager;
 using SmartCharging.API.Requests.ChargeStation;
 using SmartCharging.API.Requests.Connector;
 using SmartCharging.API.Requests.Group;
-using SmartCharging.API.Response;
-using SmartCharging.Domain.DTOs;
 
 namespace SmartCharging.API.Controllers;
 
@@ -22,25 +20,25 @@ public class SmartChargingController : ControllerBase
     #region Group
 
     [HttpGet("{id:guid}/group")]
-    public async Task<SmartChargingApiResponse<GroupDto>> GetGroup(Guid id)
+    public async Task<IResult> GetGroup(Guid id)
     {
         return await _groupManager.GetGroup(id);
     }
     
     [HttpPost("group")]
-    public async Task<SmartChargingApiResponse<GroupDto>> CreateGroup([FromBody] CreateGroupRequest request)
+    public async Task<IResult> CreateGroup([FromBody] CreateGroupRequest request)
     {
         return await _groupManager.CreateGroup(request);
     }
     
     [HttpPut("{id:guid}/group")]
-    public async Task<SmartChargingApiResponse<GroupDto>> UpdateGroup(Guid id, [FromBody] UpdateGroupRequest request)
+    public async Task<IResult> UpdateGroup(Guid id, [FromBody] UpdateGroupRequest request)
     {
         return await _groupManager.UpdateGroup(id, request);
     }
     
     [HttpDelete("{id:guid}/group")]
-    public async Task<SmartChargingApiResponse<GroupDto>> DeleteGroup(Guid id)
+    public async Task<IResult> DeleteGroup(Guid id)
     {
         return await _groupManager.DeleteGroup(id);
     }
@@ -50,25 +48,25 @@ public class SmartChargingController : ControllerBase
     #region ChargeStation
     
     [HttpGet("{id:guid}/group/{chargeStationId:guid}/chargeStation")]
-    public async Task<SmartChargingApiResponse<ChargeStationDto>> GetChargeStation(Guid id, Guid chargeStationId)
+    public async Task<IResult> GetChargeStation(Guid id, Guid chargeStationId)
     {
         return await _groupManager.GetChargeStation(id, chargeStationId);
     }
     
     [HttpPost("{id:guid}/group/chargeStation")]
-    public async Task<SmartChargingApiResponse<ChargeStationDto>> CreateChargeStation(Guid id, [FromBody] CreateChargeStationRequest request)
+    public async Task<IResult> CreateChargeStation(Guid id, [FromBody] CreateChargeStationRequest request)
     {
         return await _groupManager.CreateChargeStation(id, request);
     }
     
     [HttpPut("{id:guid}/group/{chargeStationId:guid}/chargeStation")]
-    public async Task<SmartChargingApiResponse<ChargeStationDto>> UpdateChargeStation(Guid id, Guid chargeStationId, [FromBody] UpdateChargeStationRequest request)
+    public async Task<IResult> UpdateChargeStation(Guid id, Guid chargeStationId, [FromBody] UpdateChargeStationRequest request)
     {
         return await _groupManager.UpdateChargeStation(id, chargeStationId, request);
     }
     
     [HttpDelete("{id:guid}/group/{chargeStationId:guid}/chargeStation")]
-    public async Task<SmartChargingApiResponse<ChargeStationDto>> DeleteChargeStation(Guid id, Guid chargeStationId)
+    public async Task<IResult> DeleteChargeStation(Guid id, Guid chargeStationId)
     {
         return await _groupManager.DeleteChargeStation(id, chargeStationId);
     }
@@ -78,26 +76,26 @@ public class SmartChargingController : ControllerBase
     #region Connector
     
     [HttpGet("{id:guid}/group/{chargeStationId:guid}/chargeStation/{connectorId:int}/connector")]
-    public async Task<SmartChargingApiResponse<ConnectorDto>> GetConnector(Guid id, Guid chargeStationId, int connectorId)
+    public async Task<IResult> GetConnector(Guid id, Guid chargeStationId, int connectorId)
     {
         return await _groupManager.GetConnector(id, chargeStationId, connectorId);
     }
     
     [HttpPost("{id:guid}/group/{chargeStationId:guid}/chargeStation/connector")]
-    public async Task<SmartChargingApiResponse<ConnectorDto>> CreateConnector(Guid id, Guid chargeStationId, CreateConnectorRequest request)
+    public async Task<IResult> CreateConnector(Guid id, Guid chargeStationId, CreateConnectorRequest request)
     {
         return await _groupManager.CreateConnector(id, chargeStationId,request);
     }
     
     [HttpPut("{id:guid}/group/{chargeStationId:guid}/chargeStation/{connectorId:int}/connector")]
-    public async Task<SmartChargingApiResponse<ConnectorDto>> UpdateConnector(Guid id, Guid chargeStationId, 
+    public async Task<IResult> UpdateConnector(Guid id, Guid chargeStationId, 
         int connectorId, [FromBody] UpdateConnectorRequest request)
     {
         return await _groupManager.UpdateConnector(id, chargeStationId, connectorId, request);
     }
     
     [HttpDelete("{id:guid}/group/{chargeStationId:guid}/chargeStation/{connectorId:int}/connector")]
-    public async Task<SmartChargingApiResponse<ConnectorDto>> DeleteConnector(Guid id, Guid chargeStationId, int connectorId)
+    public async Task<IResult> DeleteConnector(Guid id, Guid chargeStationId, int connectorId)
     {
         return await _groupManager.DeleteConnector(id, chargeStationId, connectorId);
     }
--- SmartCharging.API/Manager/GroupManager.cs
+++ SmartCharging.API/Manager/GroupManager.cs
@@ -2,7 +2,6 @@ using AutoMapper;
 using SmartCharging.API.Requests.ChargeStation;
 using SmartCharging.API.Requests.Connector;
 using SmartCharging.API.Requests.Group;
-using SmartCharging.API.Response;
 using SmartCharging.Domain.DTOs;
 using SmartCharging.Domain.Entities;
 using SmartCharging.Service.Contracts;
@@ -23,26 +22,26 @@ public class GroupManager
 
     #region Group
 
-    public async Task<SmartChargingApiResponse<GroupDto>> GetGroup(Guid id)
+    public async Task<IResult> GetGroup(Guid id)
     {
         var group = await _groupService.Get(id);
         return group == null 
-            ? new SmartChargingApiResponse<GroupDto>(message: "Given Group could not be found") 
-            : new SmartChargingApiResponse<GroupDto>(data: _mapper.Map<GroupDto>(group));
+            ? Results.NotFound("Given Group could not be found") 
+            : Results.Ok(_mapper.Map<GroupDto>(group));
     }
 
-    public async Task<SmartChargingApiResponse<GroupDto>> CreateGroup(CreateGroupRequest request)
+    public async Task<IResult> CreateGroup(CreateGroupRequest request)
     {
         var group = _mapper.Map<Group>(request);
         var entity = await _groupService.Create(group);
-        return new SmartChargingApiResponse<GroupDto>(_mapper.Map<GroupDto>(entity));
+        return Results.Created("uri",_mapper.Map<GroupDto>(entity));
     }
     
-    public async Task<SmartChargingApiResponse<GroupDto>> UpdateGroup(Guid id, UpdateGroupRequest request)
+    public async Task<IResult> UpdateGroup(Guid id, UpdateGroupRequest request)
     {
         var group = await _groupService.Get(id);
         if (group == null)
-            return new SmartChargingApiResponse<GroupDto>(message: "Given Group could not be found");
+            return Results.NotFound("Given Group could not be found");
 
         // TODO:Automapper couldn't handle nullable integer value condition and mapped CapacityInAmps as always 0 when the request did not have.
         if (request.Name != null)
@@ -53,44 +52,44 @@ public class GroupManager
         
         // var updatedEntity = _mapper.Map(request, group);
         var returnedEntity = _groupService.Update(group);
-        return new SmartChargingApiResponse<GroupDto>(_mapper.Map<GroupDto>(returnedEntity));
+        return Results.Ok(_mapper.Map<GroupDto>(returnedEntity));
     }
     
-    public async Task<SmartChargingApiResponse<GroupDto>> DeleteGroup(Guid id)
+    public async Task<IResult> DeleteGroup(Guid id)
     {
         var group = await _groupService.Get(id);
         if (group == null)
-            return new SmartChargingApiResponse<GroupDto>(message: "Given Group could not be found");
+            return Results.NotFound("Given Group could not be found");
         var deletedEntry = await _groupService.Delete(group);
-        return new SmartChargingApiResponse<GroupDto>(_mapper.Map<GroupDto>(deletedEntry));
+        return Results.Ok(_mapper.Map<GroupDto>(deletedEntry));
     }
     
     #endregion
 
     #region ChargeStation
 
-    public async Task<SmartChargingApiResponse<ChargeStationDto>> GetChargeStation(Guid id, Guid chargeStationId)
+    public async Task<IResult> GetChargeStation(Guid id, Guid chargeStationId)
     {
         var group = await _groupService.Get(id);
         if (group == null)
-            return new SmartChargingApiResponse<ChargeStationDto>(message: "Given Group could not be found");
+            return Results.NotFound("Given Group could not be found");
 
         var chargeStation = group.ChargeStations.FirstOrDefault(x => x.Id == chargeStationId);
         return chargeStation == null 
-            ? new SmartChargingApiResponse<ChargeStationDto>(message: "Given ChargeStation could not be found") 
-            : new SmartChargingApiResponse<ChargeStationDto>(data: _mapper.Map<ChargeStationDto>(chargeStation));
+            ? Results.NotFound("Given ChargeStation could not be found") 
+            : Results.Ok(_mapper.Map<ChargeStationDto>(chargeStation));
     }
     
-    public async Task<SmartChargingApiResponse<ChargeStationDto>> CreateChargeStation(Guid id, CreateChargeStationRequest request)
+    public async Task<IResult> CreateChargeStation(Guid id, CreateChargeStationRequest request)
     {
         var group = await _groupService.Get(id);
         if (group == null)
-            return new SmartChargingApiResponse<ChargeStationDto>(message: "Given Group could not be found");
+            return Results.NotFound("Given Group could not be found");
 
         // Get current capacity and check for max
         var currentCapacity = GetCurrentCapacityOfGroup(group);
         if (currentCapacity + request.ConnectorMaxCurrentInAmps > group.CapacityInAmps)
-            return new SmartChargingApiResponse<ChargeStationDto>(message: "Given ChargeStation could not be added. Reason: Capacity exceeded");
+            return Results.UnprocessableEntity("Given ChargeStation could not be added. Reason: Capacity exceeded");
         
         var chargeStation = new ChargeStation
         {
@@ -107,75 +106,75 @@ public class GroupManager
         
         group.ChargeStations.Add(chargeStation);
         _groupService.Update(group);
-        return new SmartChargingApiResponse<ChargeStationDto>(data:_mapper.Map<ChargeStationDto>(chargeStation));
+        return Results.Created("chargeStation",_mapper.Map<ChargeStationDto>(chargeStation));
     }
     
-    public async Task<SmartChargingApiResponse<ChargeStationDto>> UpdateChargeStation(Guid id, 
+    public async Task<IResult> UpdateChargeStation(Guid id, 
         Guid chargeStationId, UpdateChargeStationRequest request)
     {
         var group = await _groupService.Get(id);
         if (group == null)
-            return new SmartChargingApiResponse<ChargeStationDto>(message: "Given Group could not be found");
+            return Results.NotFound("Given Group could not be found");
 
         var chargeStation = group.ChargeStations.FirstOrDefault(x => x.Id == chargeStationId);
         if (chargeStation == null)
-            return new SmartChargingApiResponse<ChargeStationDto>(message: "Given ChargeStation could not be found");
+            return Results.NotFound("Given ChargeStation could not be found");
         
         // TODO: Handle with automapper
         chargeStation.Name = request.Name;
 
         _groupService.Update(group);
-        return new SmartChargingApiResponse<ChargeStationDto>(data:_mapper.Map<ChargeStationDto>(chargeStation));
+        return Results.Ok(_mapper.Map<ChargeStationDto>(chargeStation));
     }
     
-    public async Task<SmartChargingApiResponse<ChargeStationDto>> DeleteChargeStation(Guid id, Guid chargeStationId)
+    public async Task<IResult> DeleteChargeStation(Guid id, Guid chargeStationId)
     {
         var group = await _groupService.Get(id);
         if (group == null)
-            return new SmartChargingApiResponse<ChargeStationDto>(message: "Given Group could not be found");
+            return Results.NotFound("Given Group could not be found");
 
         var chargeStation = group.ChargeStations.FirstOrDefault(x => x.Id == chargeStationId);
         if (chargeStation == null)
-            return new SmartChargingApiResponse<ChargeStationDto>(message: "Given ChargeStation could not be found");
+            return Results.NotFound("Given ChargeStation could not be found");
 
         group.ChargeStations.Remove(chargeStation);
         _groupService.Update(group);
-        return new SmartChargingApiResponse<ChargeStationDto>(data:_mapper.Map<ChargeStationDto>(chargeStation));
+        return Results.Ok(_mapper.Map<ChargeStationDto>(chargeStation));
     }
     
     #endregion
     #region Connector
 
-    public async Task<SmartChargingApiResponse<ConnectorDto>> GetConnector(Guid id, Guid chargeStationId, int connectorId)
+    public async Task<IResult> GetConnector(Guid id, Guid chargeStationId, int connectorId)
     {
         var group = await _groupService.Get(id);
         if (group == null)
-            return new SmartChargingApiResponse<ConnectorDto>(message: "Given Group could not be found");
+            return Results.NotFound("Given Group could not be found");
 
         var chargeStation = group.ChargeStations.FirstOrDefault(x => x.Id == chargeStationId);
         if (chargeStation == null)
-            return new SmartChargingApiResponse<ConnectorDto>(message: "Given ChargeStation could not be found");
+            return Results.NotFound("Given ChargeStation could not be found");
 
         var connector = chargeStation.Connectors.FirstOrDefault(x => x.Id == connectorId);
         return connector == null
-            ? new SmartChargingApiResponse<ConnectorDto>(message: "Given Connector could not be found") 
-            : new SmartChargingApiResponse<ConnectorDto>(data: _mapper.Map<ConnectorDto>(connector));
+            ? Results.NotFound("Given Connector could not be found") 
+            : Results.Ok(_mapper.Map<ConnectorDto>(connector));
     }
     
-    public async Task<SmartChargingApiResponse<ConnectorDto>> CreateConnector(Guid id, Guid chargeStationId, CreateConnectorRequest request)
+    public async Task<IResult> CreateConnector(Guid id, Guid chargeStationId, CreateConnectorRequest request)
     {
         var group = await _groupService.Get(id);
         if (group == null)
-            return new SmartChargingApiResponse<ConnectorDto>(message: "Given Group could not be found");
+            return Results.NotFound("Given Group could not be found");
 
         var chargeStation = group.ChargeStations.FirstOrDefault(x => x.Id == chargeStationId);
         if (chargeStation == null)
-            return new SmartChargingApiResponse<ConnectorDto>(message: "Given ChargeStation could not be found");
+            return Results.NotFound("Given ChargeStation could not be found");
 
         //Check for max amps
         var currentAmps = GetCurrentCapacityOfGroup(group);
         if(currentAmps + request.MaxCurrentInAmps > group.CapacityInAmps)
-            return new SmartChargingApiResponse<ConnectorDto>(message: "Given Connector could not be added. Reason: Capacity exceeded");
+            return Results.UnprocessableEntity("Given Connector could not be added. Reason: Capacity exceeded");
 
         var connectorId = FindAvailableSlotForConnector(chargeStation);
         if(connectorId == 0)
@@ -188,56 +187,56 @@ public class GroupManager
         
         chargeStation.Connectors.Add(connector);
         _groupService.Update(group);
-        return new SmartChargingApiResponse<ConnectorDto>(data:_mapper.Map<ConnectorDto>(connector));
+        return Results.Ok(_mapper.Map<ConnectorDto>(connector));
     }
     
-    public async Task<SmartChargingApiResponse<ConnectorDto>> UpdateConnector(Guid id, Guid chargeStationId, 
+    public async Task<IResult> UpdateConnector(Guid id, Guid chargeStationId, 
         int connectorId, UpdateConnectorRequest request)
     {
         var group = await _groupService.Get(id);
         if (group == null)
-            return new SmartChargingApiResponse<ConnectorDto>(message: "Given Group could not be found");
+            return Results.NotFound("Given Group could not be found");
 
         var chargeStation = group.ChargeStations.FirstOrDefault(x => x.Id == chargeStationId);
         if (chargeStation == null)
-            return new SmartChargingApiResponse<ConnectorDto>(message: "Given ChargeStation could not be found");
+            return Results.NotFound("Given ChargeStation could not be found");
 
         var connector = chargeStation.Connectors.FirstOrDefault(x => x.Id == connectorId);
         if (connector == null)
-            return new SmartChargingApiResponse<ConnectorDto>(message: "Given Connector could not be found");
+            return Results.NotFound("Given Connector could not be found");
        
         //Check for max amps
         var currentAmps = GetCurrentCapacityOfGroup(group);
         if(currentAmps - connector.MaxCurrentInAmps + request.MaxCurrentInAmps > group.CapacityInAmps)
-            return new SmartChargingApiResponse<ConnectorDto>(message: "Given Connector could not be updated. Reason: Capacity exceeded");
+            return Results.UnprocessableEntity("Given Connector could not be updated. Reason: Capacity exceeded");
 
         connector.MaxCurrentInAmps = request.MaxCurrentInAmps;
         _groupService.Update(group);
-        return new SmartChargingApiResponse<ConnectorDto>(data:_mapper.Map<ConnectorDto>(connector));
+        return Results.Ok(_mapper.Map<ConnectorDto>(connector));
     }
     
-    public async Task<SmartChargingApiResponse<ConnectorDto>> DeleteConnector(Guid id,
+    public async Task<IResult> DeleteConnector(Guid id,
         Guid chargeStationId, int connectorId)
     {
         var group = await _groupService.Get(id);
         if (group == null)
-            return new SmartChargingApiResponse<ConnectorDto>(message: "Given Group could not be found");
+            return Results.NotFound("Given Group could not be found");
 
         var chargeStation = group.ChargeStations.FirstOrDefault(x => x.Id == chargeStationId);
         if (chargeStation == null)
-            return new SmartChargingApiResponse<ConnectorDto>(message: "Given ChargeStation could not be found");
+            return Results.NotFound("Given ChargeStation could not be found");
 
         var connector = chargeStation.Connectors.FirstOrDefault(x => x.Id == connectorId);
         if (connector == null)
-            return new SmartChargingApiResponse<ConnectorDto>(message: "Given Connector could not be found");
+            return Results.NotFound("Given Connector could not be found");
 
         // There have to be at least one connector in the charging station. The last connector should not be removed.
         if(chargeStation.Connectors.Count == 1)
-            return new SmartChargingApiResponse<ConnectorDto>(message: "Given Connector could not be removed. Reason: It is the last connector of charge station");
+            return Results.UnprocessableEntity("Given Connector could not be removed. Reason: It is the last connector of charge station");
         
         chargeStation.Connectors.Remove(connector);
         _groupService.Update(group);
-        return new SmartChargingApiResponse<ConnectorDto>(data:_mapper.Map<ConnectorDto>(connector));
+        return Results.Ok(_mapper.Map<ConnectorDto>(connector));
     }
 
     private static int FindAvailableSlotForConnector(ChargeStation chargeStation)
