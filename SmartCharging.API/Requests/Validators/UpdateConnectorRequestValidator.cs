7b38d92910e7c247d11705e9efd1686a77e7770d
ddemirci <doruk.demirci@ichoosr.com>
2023-10-02 01:50:50 +0200

    typo fix

--- SmartCharging.API/Requests/Validators/UpdateConnectorRequestValidator.cs
+++ SmartCharging.API/Requests/Validators/UpdateConnectorRequestValidator.cs
@@ -1,13 +1,13 @@
 using FluentValidation;
-using SmartCharging.API.Requests.Group;
+using SmartCharging.API.Requests.Connector;
 
 namespace SmartCharging.API.Requests.Validators;
 
-public class UpdateConnectorRequestValidator : AbstractValidator<UpdateGroupRequest>
+public class UpdateConnectorRequestValidator : AbstractValidator<UpdateConnectorRequest>
 {
     public UpdateConnectorRequestValidator()
     {
-        RuleFor(x => x.CapacityInAmps).GreaterThan(0)
-            .WithMessage("CapacityInAmps have to be an integer and greater than 0.");
+        RuleFor(x => x.MaxCurrentInAmps).GreaterThan(0)
+            .WithMessage("MaxCurrentInAmps have to be an integer and greater than 0.");
     }
 }
